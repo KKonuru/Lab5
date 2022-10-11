cd1241fec5b801530a1c718c75e0efdb994a6aaa
KKonuru <87139729+KKonuru@users.noreply.github.com>
2022-10-11 19:14:54 -0400

    Update analyzer.cpp

--- analyzer.cpp
+++ analyzer.cpp
@@ -36,21 +36,42 @@ int binary_search(vector<string> container, string element) {
 int main() {
     cout << "Linear search for ""not_here"" is being tested.";
     long long t1 = systemTimeNanoseconds();
-    cout << (linear_search(getStringData(), "not_here"));
+    cout << (linear_search(getStringData(), "not_here"))<< " ";
     long long t2 = systemTimeNanoseconds();
-    cout << (t2 - t1)
+    cout << (t2 - t1)<< "\n";
 
     cout << "Binary search for ""not_here"" is being tested.";
     long long t3 =  systemTimeNanoseconds();
-    cout << (binary_search(getStringData(), "not_here"));
+    cout << (binary_search(getStringData(), "not_here"))<< " ";
     long long t4 =  systemTimeNanoseconds();
-    cout << (t4 - t3);
+    cout << (t4 - t3)<< "\n";
 
     cout << "Linear search for ""mzzzz"" is being tested.";
     long long t5 =  systemTimeNanoseconds();
-    cout << (linear_search(getStringData(), "mzzzz"));
+    cout << (linear_search(getStringData(), "mzzzz"))<< " ";
     long long t6 =   systemTimeNanoseconds();
-    cout << (t6 - t5)
+    cout << (t6 - t5)<< "\n";
+
+    cout << "Binary search for ""mzzzz"" is being tested.";
+    long long t7 =  systemTimeNanoseconds();
+    cout << (binary_search(getStringData(), "mzzzz"))<< " ";
+    long long t8 =   systemTimeNanoseconds();
+    cout << (t8 - t7)<< "\n";
+
+    cout << "Linear search for ""aaaaa"" is being tested.";
+    long long t9 =  systemTimeNanoseconds();
+    cout << (linear_search(getStringData(), "aaaaa"))<< " ";
+    long long t10 =   systemTimeNanoseconds();
+    cout << (t10 - t9)<< "\n";
+
+    cout << "Binary search for ""aaaaa"" is being tested.";
+    long long t11 =  systemTimeNanoseconds();
+    cout << (binary_search(getStringData(), "aaaaaa"))<< " ";
+    long long t12 =   systemTimeNanoseconds();
+    cout << (t12 - t11)<< "\n";
+    
+    
+        
 
     return 0;
 }
